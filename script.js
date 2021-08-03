aa16f4549294a67588d40839cdade334278a154e
Dương <70809746+Duong-pog@users.noreply.github.com>
2021-08-03 13:53:25 +0700

    commit

---
+++ script.js
@@ -0,0 +1,78 @@
+let correct = document.getElementById("correct")
+let incorrect = document.getElementById("incorrect")
+let picture = document.getElementById("picture")
+let phrase = 0
+
+correct.addEventListener("click",function(){
+    if(phrase == 0){
+        picture.src = "img/4.jpg"
+        correct.innerText = "Cậu nói đi, tớ đang nghe nè 😚"
+        incorrect.innerText = "Có nói nhanh không thì bảo 😑"
+        incorrect.style.top = 0
+        incorrect.style.right = 0
+        phrase = 1
+    }
+    else if(phrase == 1){
+        picture.src = "img/3.jpg"
+        correct.innerText = "Aww, dễ thương quá, tớ cũng yêu cậu 😍"
+        incorrect.innerText = "Ừ, tớ biết rồi 🥱"
+        incorrect.style.top = 0
+        incorrect.style.right = 0
+        phrase = 2
+    }
+    else if(phrase == 2){
+        picture.src = "img/5.jpg"
+        correct.innerText = "Tớ cũng nhớ cậu, ráng chờ hết dịch tớ cho cậu ôm nha 🥺"
+        incorrect.innerText = "Ai cho ôm mà ôm, né xa tui ra 🙄"
+        incorrect.style.top = 0
+        incorrect.style.right = 0
+        phrase = 3
+    }
+    else if(phrase ==3){
+        picture.src = "img/6.jpg"
+        correct.innerText = "Em yeeu anh 😳"
+        incorrect.innerText = "Xỉu ra đấy kệ bạn 🙄"
+        incorrect.style.top = 0
+        incorrect.style.right = 0
+        phrase = 4
+    }
+    else if(phrase ==4){
+        picture.src = "img/7.jpg"
+        correct.innerText = "Dạ, anh lớp trưởng lớp em ạ 🤧"
+        incorrect.innerText = "Anh hàng xóm đấy ờ ờ làm gì nhau 😒"
+        incorrect.style.top = 0
+        incorrect.style.right = 0
+        phrase = 5
+    }
+    else if(phrase ==5){
+        picture.src = "img/8.jpg"
+        correct.innerText = "Hà cũng sẽ không yêu ai ngoài Dương đâu ❤"
+        incorrect.innerText = "Ừm 🙄"
+        incorrect.style.top = 0
+        incorrect.style.right = 0
+        phrase = 6
+    }
+    else if(phrase = 6){
+        if (confirm("Tới đây là hết rùi, ý tưởng ko có nên làm vui vui thôi ;3 . Cảm ơn Hà nha!!! Yêu Hà!!!")){
+            window.location.href = "http://quotesbae.com/wp-content/uploads/2018/01/Funny-I-Love-You-Meme-Joke-Image-14.jpg";
+        }
+    }
+})
+
+
+
+incorrect.addEventListener("mouseenter",function(event){
+    let x = Math.floor(Math.random() * 500 )
+    let y = Math.floor(Math.random() * 500 )
+    
+    incorrect.style.top = x+"px"
+    incorrect.style.right = y+"px"
+})
+
+incorrect.addEventListener("click",function(){
+    let x = Math.floor(Math.random() * 500 )
+    let y = Math.floor(Math.random() * 500 )
+    
+    incorrect.style.top = x+"px"
+    incorrect.style.right = y+"px"
+})
